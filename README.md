c11dcb476810fcd07042464fd2ebd8f14d5a61cf
ethanent <futuristiccake@gmail.com>
2020-11-21 21:37:25 -0800

    update README, rearrange source

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # domaintaken
-CLI which checks if a domain is taken via Cloudflare DoH
+> CLI which checks if a domain is taken via Cloudflare DoH
+
+## Install
+
+```sh
+go get -u github.com/ethanent/domaintaken
+go install github.com/ethanent/domaintaken
+```
+
+## Usage
+
+```sh
+domaintaken ethanent.me example.com google.com ...
+```
--- main.go
+++ main.go
@@ -7,33 +7,6 @@ import (
 	"sync"
 )
 
-func checkDomain(d string, wg *sync.WaitGroup) {
-	if !validateDomainTLD(d) {
-		color.New(color.BgHiRed, color.FgBlack).Print(" ")
-		fmt.Println(d, "INVALID TLD")
-
-		wg.Done()
-		return
-	}
-
-	exist, err := domainExists(d)
-
-	if err != nil {
-		color.New(color.BgHiRed, color.FgBlack).Print(" ")
-		fmt.Println(d, "ERR", err)
-	} else {
-		if exist {
-			color.New(color.BgRed, color.FgBlack).Print(" ")
-			fmt.Println(d, "TAKEN")
-		} else {
-			color.New(color.BgGreen, color.FgBlack).Print(" ")
-			fmt.Println(d, "AVAIL")
-		}
-	}
-
-	wg.Done()
-}
-
 func main() {
 	domains := os.Args[1:]
 
@@ -46,4 +19,35 @@ func main() {
 	}
 
 	wg.Wait()
+}
+
+func checkDomain(d string, wg *sync.WaitGroup) {
+	if !validateDomainTLD(d) {
+		color.New(color.BgHiRed, color.FgBlack).Print(" ")
+		fmt.Print(" ")
+		fmt.Println(d, "INVALID TLD")
+
+		wg.Done()
+		return
+	}
+
+	exist, err := domainExists(d)
+
+	if err != nil {
+		color.New(color.BgHiRed, color.FgBlack).Print(" ")
+		fmt.Print(" ")
+		fmt.Println(d, "ERR", err)
+	} else {
+		if exist {
+			color.New(color.BgRed, color.FgBlack).Print(" ")
+			fmt.Print(" ")
+			fmt.Println(d, "TAKEN")
+		} else {
+			color.New(color.BgGreen, color.FgBlack).Print(" ")
+			fmt.Print(" ")
+			fmt.Println(d, "AVAIL")
+		}
+	}
+
+	wg.Done()
 }
